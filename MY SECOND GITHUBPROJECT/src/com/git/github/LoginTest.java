054f675aa6350aaa6041b911026eaa9fde004931
hp <hp@192.168.23.1>
2022-11-20 09:26:23 +0530

    second commit

--- MY SECOND GITHUBPROJECT/src/com/git/github/LoginTest.java
+++ MY SECOND GITHUBPROJECT/src/com/git/github/LoginTest.java
@@ -1,24 +1,9 @@
 package com.git.github;
 
-import org.testng.annotations.Test;
 
 public class LoginTest 
-{
-	@Test
-	public void loginByEmail()
-	{
-		System.out.println("loginByEmail loginSuccessful");
-	}
-	@Test
-	public void loginByFacebook()
-	{
-		System.out.println("loginByFacebook loginSuccessful");
-	}
-	
-	@Test
-	public void loginByTwitter()
-	{
-		System.out.println("loginByTwitter loginSuccessful");
-	}
-
+{	
+public static void main(String[] args) {
+	System.out.println("HI");
+}
 }
